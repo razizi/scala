390012f89df4bbd61938bcc0a154128eac79235f
Rasoul Azizi <56647072+razizi@users.noreply.github.com>
2019-11-13 05:26:35 +0330

    Update main.scala

--- sec/main/scala/example/main.scala
+++ sec/main/scala/example/main.scala
@@ -1,9 +1,5 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
+object Hello extends App {
+  println("Hello")
 }
